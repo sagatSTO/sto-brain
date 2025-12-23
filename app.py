9d22f505418de7008a4c93d7a69b5a2ae8992f52
sagatSTO <saguiorelio32@gmail.com>
2025-12-23 12:00:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 
 app = Flask(__name__)
 
-ADMIN_EMAIL = "TON_EMAIL_saguiorelio32@gmail.com
+ADMIN_EMAIL = ADMIN_EMAIL = "saguiorelio32@gmail.com"
 
 sto_state = {
     "mode": "OBSERVATION",
